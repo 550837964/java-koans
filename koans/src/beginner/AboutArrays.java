1d6c95c293245e04d87cf1c110a9d7376efd806d
hkdychun <hkdychun@qq.com>
2018-04-19 15:36:51 +0800

    数组取值
    meh取到超出数组范围的位置了

--- koans/src/beginner/AboutArrays.java
+++ koans/src/beginner/AboutArrays.java
@@ -57,15 +57,15 @@ public class AboutArrays {
     @Koan
     public void arraysAreIndexedAtZero() {
         int[] integers = new int[]{1, 2};
-        assertEquals(integers[0], __);
-        assertEquals(integers[1], __);
+        assertEquals(integers[0], 1);
+        assertEquals(integers[1], 2);
     }
 
     @Koan
     public void arrayIndexOutOfBounds() {
         int[] array = new int[]{1};
         @SuppressWarnings("unused")
-        int meh = array[1]; // remember 0 based indexes, 1 is the 2nd element (which doesn't exist)
+        int meh = array[0]; // remember 0 based indexes, 1 is the 2nd element (which doesn't exist)
     }
 
     @Koan
