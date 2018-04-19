9bbd0565e3eea5f59771fadd7b478b2cc794cf7a
hkdychun <hkdychun@qq.com>
2018-04-19 11:18:05 +0800

    类型转换
    child是parent的子类，可以向上转型为父类parent
    但是parent直接定义的对象是child的父类，不能直接向下转型为子类child
    如果父类要向下转型，只能先由子类向上转型为父类，再向下转型为子类
    parentReference = (Parent) child
    childReference = (Child) parentReference

--- koans/src/beginner/AboutCasting.java
+++ koans/src/beginner/AboutCasting.java
@@ -61,10 +61,10 @@ public class AboutCasting {
     public void upcastWithInheritance() {
         Child child = new Child();
         Parent parentReference = child; // Why isn't there an explicit cast?
-        assertEquals(child instanceof Child, __);
-        assertEquals(parentReference instanceof Child, __);
-        assertEquals(parentReference instanceof Parent, __);
-        assertEquals(parentReference instanceof Grandparent, __);
+        assertEquals(child instanceof Child, true);
+        assertEquals(parentReference instanceof Child, true);
+        assertEquals(parentReference instanceof Parent, true);
+        assertEquals(parentReference instanceof Grandparent, true);
     }
 
     @Koan
@@ -72,7 +72,7 @@ public class AboutCasting {
         Child child = new Child();
         Parent parentReference = child;
         // If the result is unexpected, consider the difference between an instance and its reference
-        assertEquals(parentReference.complain(), __);
+        assertEquals(parentReference.complain(), "Are we there yet!!");
     }
 
     @Koan
@@ -80,9 +80,9 @@ public class AboutCasting {
         Grandparent child = new Child();
         Parent parentReference = (Parent) child; // Why do we need an explicit cast here?
         Child childReference = (Child) parentReference; // Or here?
-        assertEquals(childReference instanceof Child, __);
-        assertEquals(childReference instanceof Parent, __);
-        assertEquals(childReference instanceof Grandparent, __);
+        assertEquals(childReference instanceof Child, true);
+        assertEquals(childReference instanceof Parent, true);
+        assertEquals(childReference instanceof Grandparent, true);
     }
 
     @Koan
