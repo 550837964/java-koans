f660fc69f36c436b07b807f2130eaffd82d4b9be
hkdychun <hkdychun@qq.com>
2018-04-19 14:29:29 +0800

    异常其他使用方法
    对特定情况给出指定异常，否则正常运行

--- koans/src/beginner/AboutExceptions.java
+++ koans/src/beginner/AboutExceptions.java
@@ -143,7 +143,7 @@ public class AboutExceptions {
         } catch (IllegalArgumentException ex) {
             s = "caught an IllegalArgumentException";
         }
-        assertEquals(s, __);
+        assertEquals(s, "caught an IllegalArgumentException");
     }
 
     @Koan
@@ -155,7 +155,7 @@ public class AboutExceptions {
         } catch (IllegalArgumentException ex) {
             s = "caught an IllegalArgumentException";
         }
-        assertEquals(s, __);
+        assertEquals(s, 5);
     }
 
     private int validateUsingIllegalArgumentException(String str) {
