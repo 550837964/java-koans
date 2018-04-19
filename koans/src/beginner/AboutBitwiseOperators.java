d76fb3c452fa2fe08402bc56c1d76c8e4b2b425c
hkdychun <hkdychun@qq.com>
2018-04-19 15:00:13 +0800

    逻辑运算

--- koans/src/beginner/AboutBitwiseOperators.java
+++ koans/src/beginner/AboutBitwiseOperators.java
@@ -11,14 +11,14 @@ public class AboutBitwiseOperators {
     public void fullAnd() {
         int i = 1;
         if (true & (++i < 8)) i = i + 1;
-        assertEquals(i, __);
+        assertEquals(i, 3);
     }
 
     @Koan
     public void shortCircuitAnd() {
         int i = 1;
         if (true && (i < -28)) i = i + 1;
-        assertEquals(i, __);
+        assertEquals(i, 1);
     }
 
     @Koan
@@ -26,7 +26,7 @@ public class AboutBitwiseOperators {
         int i = 1;
         int a = 6;
         if ((a < 9) ^ false) i = i + 1;
-        assertEquals(i, __);
+        assertEquals(i, 2);
     }
 
     @Koan
