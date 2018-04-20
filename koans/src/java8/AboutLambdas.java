4cc99a3accc110d4d1a25501e76de6265b40bbc0
hkdychun <hkdychun@qq.com>
2018-04-20 16:02:28 +0800

    Lambdas
    修改一些默认执行的动作？
    让右箭头左边的内容更替为右箭头右边的执行内容

--- koans/src/java8/AboutLambdas.java
+++ koans/src/java8/AboutLambdas.java
@@ -31,7 +31,7 @@ public class AboutLambdas {
     public void verySimpleLambda() throws InterruptedException {
         Runnable r8 = () -> str = "changed in lambda";
         r8.run();
-        assertEquals(str, __);
+        assertEquals(str, "changed in lambda");
     }
 
     @Koan
@@ -40,7 +40,7 @@ public class AboutLambdas {
             return n.toUpperCase();
         };
         String capitalized = caps.capitalize("James");
-        assertEquals(capitalized, __);
+        assertEquals(capitalized, "JAMES");
     }
 
     @Koan
@@ -50,17 +50,17 @@ public class AboutLambdas {
         //parameter parenthesis can be omitted for single parameter lambda
         Caps caps = s -> s.toUpperCase();
         String capitalized = caps.capitalize("Arthur");
-        assertEquals(capitalized, __);
+        assertEquals(capitalized, "ARTHUR");
     }
 
     @Koan
     public void lambdaField() {
-        assertEquals(thisLambdaField.capitalize(""), __);
+        assertEquals(thisLambdaField.capitalize(""), "CAPS");
     }
 
     @Koan
     public void lambdaField2() {
-        assertEquals(toStringLambdaField.capitalize(""), __);
+        assertEquals(toStringLambdaField.capitalize(""), "CAPS");
     }
 
     @Koan
