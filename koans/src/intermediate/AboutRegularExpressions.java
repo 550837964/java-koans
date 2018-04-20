c3493d85ea3c128815979a5b2814628f562c1508
hkdychun <hkdychun@qq.com>
2018-04-20 13:21:08 +0800

    Pattern.compile输入参数
    此处“.”表达的是“任意字符”
    所以xyz，x z，xfz都可以匹配到
    [abc]     a、b 或 c（简单类）
    
    [^abc]   任何字符，除了 a、b 或 c（否定）
    
    [a-zA-Z] a到 z 或 A 到 Z，两头的字母包括在内（范围）
    
    [a-d[m-p]]   a到 d 或 m 到 p：[a-dm-p]（并集）
    
    [a-z&&[def]]       d、e 或 f（交集）
    
    [a-z&&[^bc]]      a 到 z，除了 b 和 c：[ad-z]（减去）
    
    [a-z&&[^m-p]]   a 到 z，而非 m 到 p：[a-lq-z]（减去）
    
    .      任何字符（与行结束符可能匹配也可能不匹配）
    
    \d   数字：[0-9]
    
    \D  非数字： [^0-9]
    
    \s   空白字符：[\t\n\x0B\f\r]
    
    \S   非空白字符：[^\s]
    
    \w  单词字符：[a-zA-Z_0-9]
    
    \W 非单词字符：[^\w]
    
    \p{Lower}   小写字母字符：[a-z]
    
    \p{Upper}  大写字母字符：[A-Z]
    
    X?   X，一次或一次也没有
    
    X*   X，零次或多次
    
    X+  X，一次或多次
    
    X{n}      X，恰好 n 次
    
    X{n,}     X，至少 n 次
    
    X{n,m}  X，至少 n 次，但是不超过 m 次
    
    (X) X，作为捕获组
    
    非捕获
    
    (?=X)    X，通过零宽度的正lookahead
    
    (?!X)      X，通过零宽度的负lookahead
    
    (?<=X)  X，通过零宽度的正lookbehind
    
    (?<!X)   X，通过零宽度的负lookbehind
    
    (?>X)    X，作为独立的非捕获组

--- koans/src/intermediate/AboutRegularExpressions.java
+++ koans/src/intermediate/AboutRegularExpressions.java
@@ -29,12 +29,12 @@ public class AboutRegularExpressions {
         Pattern p = Pattern.compile("x.z");
         Matcher m = p.matcher("xyz u x z u xfz");
         // index 012345678901234
-        assertEquals(m.find(), __);
-        assertEquals(m.start(), __);
-        assertEquals(m.find(), __);
-        assertEquals(m.start(), __);
-        assertEquals(m.find(), __);
-        assertEquals(m.start(), __);
+        assertEquals(m.find(), true);
+        assertEquals(m.start(), 0);
+        assertEquals(m.find(), true);
+        assertEquals(m.start(), 6);
+        assertEquals(m.find(), true);
+        assertEquals(m.start(), 12);
     }
 
     @Koan
